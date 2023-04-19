854258631d88d521300c07bf3f6b7767794e3e2d
MarcelloMuy <marcellomuylaert@icloud.com>
2023-04-19 20:06:10 +0000

    install dotenv, add imports and create requirements.txt

--- easyGrocery2/easyGrocery2/settings.py
+++ easyGrocery2/easyGrocery2/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dotenv
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4ww9e5@y3_f(=ui)s@2%7)$$=j@ae%mux87&3rd0kzx6udi3kz'
+SECRET_KEY = os.environ.get('MY_SECRET_KEY', 'default_value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
