71e7e8e77cd213367be1e6e6b67770c48bcb065c
MarcelloMuy <marcellomuylaert@icloud.com>
2023-04-20 21:39:17 +0000

    install allauth and move easyGrocery2 files one folder down

--- easyGrocery2/settings.py
+++ easyGrocery2/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
